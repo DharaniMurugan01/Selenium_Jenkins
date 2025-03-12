2ec649c7c5a4498b43ba7623caff029b15959ef3
DharaniMurugan01 <2k21dhara@gmail.com>
2025-03-12 07:22:57 +0530

    commited

--- Selenium_Jenkins/src/main/java/com/kiot/Views.java
+++ Selenium_Jenkins/src/main/java/com/kiot/Views.java
@@ -5,14 +5,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Views {
 
 	public static void main(String[] args) throws InterruptedException {
-		WebDriver driver=new ChromeDriver();
-		
+		 WebDriverManager.edgedriver().setup();
+	     WebDriver driver = new EdgeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://configureselenium.blogspot.com/2020/11/selenium-tutorials.html");
 		WebElement ele2=driver.findElement(By.cssSelector("#Stats1_totalCount"));
